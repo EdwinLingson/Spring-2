aa69dca83bb84c0667dcfb035f7bec19f6c3e08d
Sujitha Ponnan <A-5356@IBSPLC.COM>
2019-01-10 11:57:32 +0530

    Second

---
+++ src/main/java/com/edwin/chains/Hotels/HotelsApplication.java
@@ -0,0 +1,16 @@
+package com.edwin.chains.Hotels;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelsApplication {
+
+	public static void main(String[] args) {
+		//SpringApplication.run(HotelsApplication.class, args);
+		System.out.println("Hello world");
+		System.out.println(4+"s");
+	}
+
+}
+
